1b1b7a0444928d2f32e1ae28732c4132196b0f29
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2022-09-23 13:19:29 +0200

    Defined basic app navigation routes

---
+++ app/src/main/java/com/andrii_a/walleria/ui/navigation/NavigationScreen.kt
@@ -0,0 +1,46 @@
+package com.andrii_a.walleria.ui.navigation
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Collections
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.Topic
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Collections
+import androidx.compose.material.icons.outlined.Photo
+import androidx.compose.material.icons.outlined.Topic
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.andrii_a.walleria.R
+
+enum class NavigationScreen(
+    val route: String,
+    @StringRes val titleRes: Int,
+    val iconUnselected: ImageVector,
+    val iconSelected: ImageVector
+) {
+    Photos(
+        route = "photos",
+        titleRes = R.string.photos,
+        iconUnselected = Icons.Outlined.Photo,
+        iconSelected = Icons.Filled.Photo
+    ),
+    Collections(
+        route = "collections",
+        titleRes = R.string.collections,
+        iconUnselected = Icons.Outlined.Collections,
+        iconSelected = Icons.Filled.Collections
+    ),
+    Topics(
+        route = "topics",
+        titleRes = R.string.topics,
+        iconUnselected = Icons.Outlined.Topic,
+        iconSelected = Icons.Filled.Topic
+    ),
+    Profile(
+        route = "profile",
+        titleRes = R.string.profile,
+        iconUnselected = Icons.Outlined.AccountCircle,
+        iconSelected = Icons.Filled.AccountCircle
+    )
+}
---
+++ app/src/main/java/com/andrii_a/walleria/ui/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package com.andrii_a.walleria.ui.navigation
+
+enum class Screen(val route: String) {
+    PhotoDetails("photo_details"),
+    CollectionDetails("collection_details"),
+    TopicDetails("topic_details"),
+    UserDetails("user_details"),
+    Search("search"),
+    AddPhotoToCollection("add_photo_to_collection"),
+    EditUserProfile("edit_user_profile"),
+    Settings("settings"),
+    About("about")
+}
