89d6446c62766949ed21c3fb1afd794a9a4e8b17
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2022-09-26 13:45:14 +0200

    Initial MainActivity implementation

---
+++ app/src/main/java/com/andrii_a/walleria/ui/main/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.andrii_a.walleria.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.navigation.compose.rememberNavController
+import com.andrii_a.walleria.ui.navigation.MainNavHost
+import com.andrii_a.walleria.ui.theme.WalleriaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            WalleriaTheme {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val navController = rememberNavController()
+
+                    MainNavHost(navHostController = navController)
+                }
+            }
+        }
+    }
+}
