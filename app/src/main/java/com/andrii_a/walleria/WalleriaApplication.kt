18654191caa3e32c90d9b938c3a724e35aadcf52
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2022-09-26 13:48:23 +0200

    Created WalleriaApplication.kt to initiate hilt dependency injection and application coroutine scope

---
+++ app/src/main/java/com/andrii_a/walleria/WalleriaApplication.kt
@@ -0,0 +1,21 @@
+package com.andrii_a.walleria
+
+import android.app.Application
+import com.andrii_a.walleria.core.ApplicationScope
+import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import javax.inject.Inject
+
+@HiltAndroidApp
+class WalleriaApplication : Application() {
+
+    @Inject
+    @ApplicationScope
+    lateinit var applicationScope: CoroutineScope
+
+    override fun onTerminate() {
+        applicationScope.cancel()
+        super.onTerminate()
+    }
+}
