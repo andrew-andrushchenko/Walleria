185f573faef62b6582b103cc77f3ff05d5efd98b
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2022-12-08 20:43:10 +0100

    Navigation bar icons replaced

--- app/src/main/java/com/andrii_a/walleria/ui/navigation/NavigationScreen.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/navigation/NavigationScreen.kt
@@ -1,47 +1,44 @@
 package com.andrii_a.walleria.ui.navigation
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.*
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.andrii_a.walleria.R
 
 enum class NavigationScreen(
     val route: String,
     @StringRes val titleRes: Int,
-    val iconUnselected: ImageVector,
-    val iconSelected: ImageVector
+    @DrawableRes val iconUnselected: Int,
+    @DrawableRes val iconSelected: Int
 ) {
     Photos(
         route = "photos",
         titleRes = R.string.photos,
-        iconUnselected = Icons.Outlined.Photo,
-        iconSelected = Icons.Filled.Photo
+        iconUnselected = R.drawable.ic_photos_outlined,
+        iconSelected = R.drawable.ic_photos_filled
     ),
     Collections(
         route = "collections",
         titleRes = R.string.collections,
-        iconUnselected = Icons.Outlined.Collections,
-        iconSelected = Icons.Filled.Collections
+        iconUnselected = R.drawable.ic_collection_outlined,
+        iconSelected = R.drawable.ic_collection_filled
     ),
     Topics(
         route = "topics",
         titleRes = R.string.topics,
-        iconUnselected = Icons.Outlined.Topic,
-        iconSelected = Icons.Filled.Topic
+        iconUnselected = R.drawable.ic_topic_outlined,
+        iconSelected = R.drawable.ic_topic_filled
     ),
     Search(
         route = "search",
         titleRes = R.string.search,
-        iconUnselected = Icons.Outlined.Search,
-        iconSelected = Icons.Filled.Search
+        iconUnselected = R.drawable.ic_search_outlined,
+        iconSelected = R.drawable.ic_search_filled
     ),
     Profile(
         route = "profile",
         titleRes = R.string.profile,
-        iconUnselected = Icons.Outlined.AccountCircle,
-        iconSelected = Icons.Filled.AccountCircle
+        iconUnselected = R.drawable.ic_user_outlined,
+        iconSelected = R.drawable.ic_user_outlined
     )
 }
 
