fa5bab2bd6f5fb7422e9770e5c7e99d909ab2a54
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-01-01 00:46:20 +0100

    Cleaned up ProfileScreen.kt, added logged out user section, renamed logged in user section composable

--- app/src/main/java/com/andrii_a/walleria/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/profile/ProfileScreen.kt
@@ -2,7 +2,9 @@ package com.andrii_a.walleria.ui.profile
 
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -54,7 +56,7 @@ fun ProfileScreen(
 
                 val userProfileData by userProfileDataStateFlow.collectAsState()
 
-                UserProfileHeader(
+                LoggedInUserSection(
                     userProfilePhotoUrl = userProfileData.profilePhotoUrl,
                     username = stringResource(
                         id = R.string.user_full_name_formatted,
@@ -62,63 +64,20 @@ fun ProfileScreen(
                         userProfileData.lastName
                     ),
                     userNickname = userProfileData.nickname,
-                    userEmail = userProfileData.email
+                    userEmail = userProfileData.email,
+                    navigateToViewProfileScreen = navigateToViewProfileScreen,
+                    navigateToEditProfileScreen = navigateToEditProfileScreen,
+                    onLogoutClick = { showLogoutConfirmationDialog = true }
                 )
 
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    WTextButton(
-                        onClick = navigateToViewProfileScreen,
-                        iconPainter = painterResource(id = R.drawable.ic_view_outlined),
-                        text = stringResource(id = R.string.user_profile_view),
-                        modifier = Modifier.weight(0.33f)
+                if (showLogoutConfirmationDialog) {
+                    LogoutConfirmationDialog(
+                        onLogout = logout,
+                        onDismiss = { showLogoutConfirmationDialog = false }
                     )
-
-                    Spacer(modifier = Modifier.padding(end = 4.dp))
-
-                    WTextButton(
-                        onClick = navigateToEditProfileScreen,
-                        iconPainter = painterResource(id = R.drawable.ic_edit_outlined),
-                        text = stringResource(id = R.string.user_profile_edit),
-                        modifier = Modifier.weight(0.33f)
-                    )
-
-                    Spacer(modifier = Modifier.padding(end = 4.dp))
-
-                    WTextButton(
-                        onClick = { showLogoutConfirmationDialog = true },
-                        iconPainter = painterResource(id = R.drawable.ic_logout_outlined),
-                        text = stringResource(id = R.string.logout),
-                        modifier = Modifier.weight(0.33f)
-                    )
-
-                    if (showLogoutConfirmationDialog) {
-                        LogoutConfirmationDialog(
-                            onLogout = logout,
-                            onDismiss = { showLogoutConfirmationDialog = false }
-                        )
-                    }
                 }
             } else {
-                Text(
-                    text = stringResource(id = R.string.login_rationale),
-                    style = MaterialTheme.typography.h6,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(8.dp)
-                )
-
-                WTextButton(
-                    onClick = navigateToLoginScreen,
-                    iconPainter = painterResource(id = R.drawable.ic_login_outlined),
-                    text = stringResource(id = R.string.login),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(64.dp)
-                )
+                LoggedOutUserSection(navigateToLoginScreen = navigateToLoginScreen)
             }
 
             Divider(
@@ -131,15 +90,19 @@ fun ProfileScreen(
                 onClick = navigateToSettingsScreen,
                 iconPainter = painterResource(id = R.drawable.ic_settings_outlined),
                 text = stringResource(id = R.string.settings),
+                shape = RoundedCornerShape(16.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(64.dp)
             )
 
+            Spacer(modifier = Modifier.padding(bottom = 4.dp))
+
             WTextButton(
                 onClick = navigateToAboutScreen,
                 iconPainter = painterResource(id = R.drawable.ic_about_outlined),
                 text = stringResource(id = R.string.about),
+                shape = RoundedCornerShape(16.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(64.dp)
@@ -149,11 +112,90 @@ fun ProfileScreen(
 }
 
 @Composable
-fun UserProfileHeader(
+fun LoggedOutUserSection(navigateToLoginScreen: () -> Unit) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        var showAddAccountSection by remember {
+            mutableStateOf(false)
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 8.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .clickable(onClick = { showAddAccountSection = !showAddAccountSection })
+                .padding(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = stringResource(id = R.string.app_name),
+                tint = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.size(64.dp)
+            )
+
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            Column {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Center
+                )
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.creation_starts_here),
+                        style = MaterialTheme.typography.subtitle2,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                    )
+
+                    Icon(
+                        painter = painterResource(
+                            id = if (showAddAccountSection) R.drawable.ic_arrow_up_alt
+                            else R.drawable.ic_arrow_down_alt
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+            }
+        }
+
+        AnimatedVisibility(visible = showAddAccountSection) {
+            WTextButton(
+                onClick = navigateToLoginScreen,
+                iconPainter = painterResource(id = R.drawable.ic_add_outlined),
+                text = stringResource(id = R.string.add_account),
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun LoggedInUserSection(
     userProfilePhotoUrl: String,
     username: String,
     userNickname: String,
-    userEmail: String
+    userEmail: String,
+    navigateToViewProfileScreen: () -> Unit,
+    navigateToEditProfileScreen: () -> Unit,
+    onLogoutClick: () -> Unit
 ) {
     Column(
         verticalArrangement = Arrangement.SpaceAround,
@@ -195,6 +237,39 @@ fun UserProfileHeader(
             overflow = TextOverflow.Ellipsis
         )
     }
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        WTextButton(
+            onClick = navigateToViewProfileScreen,
+            iconPainter = painterResource(id = R.drawable.ic_view_outlined),
+            text = stringResource(id = R.string.user_profile_view),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.weight(0.33f)
+        )
+
+        Spacer(modifier = Modifier.padding(end = 4.dp))
+
+        WTextButton(
+            onClick = navigateToEditProfileScreen,
+            iconPainter = painterResource(id = R.drawable.ic_edit_outlined),
+            text = stringResource(id = R.string.user_profile_edit),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.weight(0.33f)
+        )
+
+        Spacer(modifier = Modifier.padding(end = 4.dp))
+
+        WTextButton(
+            onClick = onLogoutClick,
+            iconPainter = painterResource(id = R.drawable.ic_logout_outlined),
+            text = stringResource(id = R.string.logout),
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.weight(0.33f)
+        )
+    }
 }
 
 @Composable
@@ -205,6 +280,7 @@ fun LogoutConfirmationDialog(
     AlertDialog(
         title = { Text(text = stringResource(id = R.string.logout)) },
         text = { Text(text = stringResource(id = R.string.logout_confirmation)) },
+        shape = RoundedCornerShape(16.dp),
         onDismissRequest = onDismiss,
         confirmButton = {
             WTextButton(
