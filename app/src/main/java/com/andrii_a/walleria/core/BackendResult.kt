a3551799ed49adc9a19e39add797dc3aa70837a1
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-07-26 15:50:29 +0200

    BackendResult is now sealed interface

--- app/src/main/java/com/andrii_a/walleria/core/BackendResult.kt
+++ app/src/main/java/com/andrii_a/walleria/core/BackendResult.kt
@@ -1,12 +1,8 @@
 package com.andrii_a.walleria.core
 
-sealed class BackendResult<out T> {
-
-    object Empty : BackendResult<Nothing>()
-
-    object Loading : BackendResult<Nothing>()
-
-    data class Success<out T>(val value: T) : BackendResult<T>()
-
-    data class Error(val code: Int? = null, val reason: String? = null) : BackendResult<Nothing>()
+sealed interface BackendResult<out T> {
+    data object Empty : BackendResult<Nothing>
+    data object Loading : BackendResult<Nothing>
+    data class Success<out T>(val value: T) : BackendResult<T>
+    data class Error(val code: Int? = null, val reason: String? = null) : BackendResult<Nothing>
 }
