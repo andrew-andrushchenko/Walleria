7e86ee59d04e39c53ec4fbbb55614448fe1607ea
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-07-26 15:12:01 +0200

    Extracted composable entry point into a separate file to cleanup MainActivity

--- app/src/main/java/com/andrii_a/walleria/ui/main/MainActivity.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/main/MainActivity.kt
@@ -3,23 +3,8 @@ package com.andrii_a.walleria.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.rememberNavController
-import com.andrii_a.walleria.ui.navigation.MainNavHost
-import com.andrii_a.walleria.ui.navigation.NavigationScreen
-import com.andrii_a.walleria.ui.navigation.NavigationScreenRoutes
-import com.andrii_a.walleria.ui.theme.WalleriaTheme
-import com.andrii_a.walleria.ui.util.currentRoute
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,39 +16,7 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-            WalleriaTheme {
-                Surface(
-                    color = MaterialTheme.colors.background,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    val systemUiController = rememberSystemUiController()
-                    val navController = rememberNavController()
-
-                    Box(modifier = Modifier.imePadding()) {
-                        MainNavHost(
-                            navHostController = navController,
-                            systemUiController = systemUiController
-                        )
-
-                        if (navController.currentRoute in NavigationScreenRoutes) {
-                            WNavigationBar(
-                                navScreenItems = NavigationScreen.values().toList(),
-                                onItemSelected = { navigationScreen ->
-                                    navController.navigate(navigationScreen.route) {
-                                        launchSingleTop = true
-                                        restoreState = true
-                                        popUpTo(navController.graph.findStartDestination().id) {
-                                            saveState = true
-                                        }
-                                    }
-                                },
-                                navController = navController,
-                                modifier = Modifier.align(Alignment.BottomCenter)
-                            )
-                        }
-                    }
-                }
-            }
+            Walleria()
         }
     }
 }
