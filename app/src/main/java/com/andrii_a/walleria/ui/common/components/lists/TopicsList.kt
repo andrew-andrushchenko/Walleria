1bf311f27b01ab1ad08dc609ec8bef1a5087ad97
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2023-10-05 13:12:56 +0200

    Refresh DefaultTopicItem look and feel.

--- app/src/main/java/com/andrii_a/walleria/ui/common/components/lists/TopicsList.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/common/components/lists/TopicsList.kt
@@ -1,30 +1,34 @@
 package com.andrii_a.walleria.ui.common.components.lists
 
+import android.content.res.Configuration
 import android.graphics.drawable.ColorDrawable
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
@@ -37,6 +41,7 @@ import com.andrii_a.walleria.R
 import com.andrii_a.walleria.domain.PhotoQuality
 import com.andrii_a.walleria.domain.TopicStatus
 import com.andrii_a.walleria.domain.models.photo.Photo
+import com.andrii_a.walleria.domain.models.photo.PhotoUrls
 import com.andrii_a.walleria.domain.models.topic.Topic
 import com.andrii_a.walleria.ui.common.TopicId
 import com.andrii_a.walleria.ui.common.components.EmptyContentBanner
@@ -44,6 +49,9 @@ import com.andrii_a.walleria.ui.common.components.ErrorBanner
 import com.andrii_a.walleria.ui.common.components.ErrorItem
 import com.andrii_a.walleria.ui.common.components.LoadingListItem
 import com.andrii_a.walleria.ui.common.components.ScrollToTopLayout
+import com.andrii_a.walleria.ui.theme.PhotoDetailsActionButtonContainerColor
+import com.andrii_a.walleria.ui.theme.PhotoDetailsActionButtonContentColor
+import com.andrii_a.walleria.ui.theme.WalleriaTheme
 import com.andrii_a.walleria.ui.util.abbreviatedNumberString
 import com.andrii_a.walleria.ui.util.getUrlByQuality
 import com.andrii_a.walleria.ui.util.ownerUsername
@@ -99,9 +107,9 @@ fun TopicsList(
                                         onClick(TopicId(topic.id))
                                     },
                                     modifier = Modifier.padding(
-                                        start = 8.dp,
-                                        end = 8.dp,
-                                        bottom = 8.dp
+                                        start = 16.dp,
+                                        end = 16.dp,
+                                        bottom = 16.dp
                                     )
                                 )
                             }
@@ -150,7 +158,6 @@ fun TopicsList(
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultTopicItem(
     title: String,
@@ -163,111 +170,167 @@ fun DefaultTopicItem(
     modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    Card(
-        shape = RoundedCornerShape(16.dp),
-        onClick = onClick,
+    ConstraintLayout(
         modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
+            .fillMaxWidth()
+            .height(150.dp)
+            .clickable(onClick = onClick)
     ) {
-        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
-            val (
-                titleText, coverPhotoAsyncImage, statusText,
-                curatorUsernameText, lastUpdatedText, totalPhotosText
-            ) = createRefs()
+        val (
+            coverPhotoImage, titleText, statusChip,
+            curatorUsernameText, lastUpdatedText, totalPhotosText
+        ) = createRefs()
 
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(coverPhoto?.getUrlByQuality(coverPhotoQuality))
-                    .crossfade(durationMillis = 1000)
-                    .placeholder(ColorDrawable(coverPhoto?.primaryColorInt ?: android.graphics.Color.GRAY))
-                    .build(),
-                contentDescription = stringResource(id = R.string.topic_cover_photo),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .constrainAs(coverPhotoAsyncImage) {
-                        top.linkTo(parent.top, margin = 12.dp)
-                        start.linkTo(parent.start, margin = 12.dp)
-                        end.linkTo(titleText.start, margin = 12.dp)
-                        bottom.linkTo(parent.bottom, margin = 12.dp)
-                    }
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(12.dp))
-            )
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(coverPhoto?.getUrlByQuality(coverPhotoQuality))
+                .crossfade(durationMillis = 1000)
+                .placeholder(
+                    ColorDrawable(
+                        coverPhoto?.primaryColorInt ?: android.graphics.Color.GRAY
+                    )
+                )
+                .build(),
+            contentDescription = stringResource(id = R.string.topic_cover_photo),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .constrainAs(coverPhotoImage) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .drawWithContent {
+                    drawContent()
+                    drawRect(color = PhotoDetailsActionButtonContentColor.copy(alpha = 0.5f))
+                }
+        )
 
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleMedium,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.constrainAs(titleText) {
-                    top.linkTo(parent.top, margin = 12.dp)
-                    start.linkTo(coverPhotoAsyncImage.end)
-                    end.linkTo(statusText.start, margin = 12.dp)
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            color = PhotoDetailsActionButtonContainerColor,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.constrainAs(titleText) {
+                top.linkTo(parent.top, 16.dp)
+                start.linkTo(parent.start, 16.dp)
+                end.linkTo(statusChip.start, 16.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+
+        Text(
+            text = stringResource(id = R.string.topic_curated_by_formatted, curatorUsername),
+            style = MaterialTheme.typography.titleSmall,
+            color = PhotoDetailsActionButtonContainerColor,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.constrainAs(curatorUsernameText) {
+                top.linkTo(titleText.bottom, 8.dp)
+                start.linkTo(parent.start, 16.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+
+        Text(
+            text = stringResource(
+                id = R.string.topic_photos_formatted,
+                totalPhotos.abbreviatedNumberString
+            ),
+            style = MaterialTheme.typography.titleMedium,
+            color = PhotoDetailsActionButtonContainerColor,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.constrainAs(totalPhotosText) {
+                bottom.linkTo(parent.bottom, 16.dp)
+                start.linkTo(parent.start, 16.dp)
+                end.linkTo(lastUpdatedText.start, 8.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+
+        Text(
+            text = updatedAt.timeAgoLocalizedString.orEmpty(),
+            style = MaterialTheme.typography.titleMedium,
+            color = PhotoDetailsActionButtonContainerColor,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.constrainAs(lastUpdatedText) {
+                end.linkTo(parent.end, 16.dp)
+                bottom.linkTo(parent.bottom, 16.dp)
+                width = Dimension.fillToConstraints
+            }
+        )
+
+        StatusChip(
+            status = status,
+            modifier = Modifier
+                .constrainAs(statusChip) {
+                    end.linkTo(parent.end, 16.dp)
+                    top.linkTo(parent.top, 4.dp)
                     width = Dimension.fillToConstraints
                 }
-            )
-
-            Text(
-                text = stringResource(id = R.string.topic_curated_by_formatted, curatorUsername),
-                style = MaterialTheme.typography.titleSmall,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.constrainAs(curatorUsernameText) {
-                    top.linkTo(titleText.bottom)
-                    start.linkTo(coverPhotoAsyncImage.end, margin = 12.dp)
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            Text(
-                text = stringResource(
-                    id = R.string.topic_photos_formatted,
-                    totalPhotos.abbreviatedNumberString
-                ),
-                style = MaterialTheme.typography.titleSmall,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.constrainAs(totalPhotosText) {
-                    top.linkTo(curatorUsernameText.bottom, margin = 12.dp)
-                    start.linkTo(coverPhotoAsyncImage.end, margin = 12.dp)
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            Text(
-                text = updatedAt.timeAgoLocalizedString.orEmpty(),
-                style = MaterialTheme.typography.titleMedium,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.constrainAs(lastUpdatedText) {
-                    end.linkTo(parent.end, margin = 12.dp)
-                    bottom.linkTo(parent.bottom, margin = 12.dp)
-                    width = Dimension.fillToConstraints
-                }
-            )
-
-            StatusIndicatorText(
-                status = status,
-                modifier = Modifier
-                    .constrainAs(statusText) {
-                        end.linkTo(parent.end, margin = 12.dp)
-                        top.linkTo(parent.top, margin = 12.dp)
-                        width = Dimension.fillToConstraints
-                    }
-            )
-        }
+        )
     }
 }
 
 @Composable
-fun StatusIndicatorText(status: TopicStatus, modifier: Modifier = Modifier) {
-    Text(
-        text = stringResource(id = status.titleRes),
-        maxLines = 1,
-        color = MaterialTheme.colorScheme.secondaryContainer,
-        overflow = TextOverflow.Ellipsis,
+fun StatusChip(
+    status: TopicStatus,
+    modifier: Modifier = Modifier,
+    color: Color = PhotoDetailsActionButtonContainerColor
+) {
+    SuggestionChip(
+        label = { Text(text = stringResource(id = status.titleRes)) },
+        onClick = {},
+        colors = SuggestionChipDefaults.suggestionChipColors(
+            labelColor = color
+        ),
+        border = SuggestionChipDefaults.suggestionChipBorder(
+            borderColor = color
+        ),
         modifier = modifier
-            .clip(RoundedCornerShape(50))
-            .background(color = MaterialTheme.colorScheme.secondary)
-            .padding(horizontal = 16.dp, vertical = 8.dp)
     )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultTopicItemPreview() {
+    WalleriaTheme {
+        val photo = Photo(
+            id = "",
+            width = 200,
+            height = 300,
+            color = "#E0E0E0",
+            blurHash = "",
+            views = 200,
+            downloads = 200,
+            likes = 10,
+            likedByUser = false,
+            description = "",
+            exif = null,
+            location = null,
+            tags = null,
+            relatedCollections = null,
+            currentUserCollections = null,
+            sponsorship = null,
+            urls = PhotoUrls("", "", "", "", ""),
+            links = null,
+            user = null
+        )
+
+        DefaultTopicItem(
+            title = "Topic title",
+            coverPhoto = photo,
+            coverPhotoQuality = PhotoQuality.MEDIUM,
+            totalPhotos = 180_000,
+            curatorUsername = "John Smith",
+            status = TopicStatus.OPEN,
+            updatedAt = "2023-09-13T10:39:35Z",
+            onClick = {}
+        )
+    }
 }
