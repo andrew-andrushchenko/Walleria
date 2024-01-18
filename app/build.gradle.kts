ecbd5e1efd3b4221bf0c3d8903980f60d43cb056
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2024-01-18 22:34:09 +0100

    Gradle and dependencies versions bump.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,9 +102,9 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
 
     // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     // Navigation + compose
     implementation("androidx.navigation:navigation-compose:2.7.6")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
 
@@ -12,7 +12,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
     }
 }
