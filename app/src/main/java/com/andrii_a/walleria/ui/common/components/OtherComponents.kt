5024a84f656286193d1884ce74a9b730778d0caf
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2025-02-12 22:12:03 +0100

    Removed redundant scroll to top component.

--- app/src/main/java/com/andrii_a/walleria/ui/common/components/OtherComponents.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/common/components/OtherComponents.kt
@@ -5,10 +5,19 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridState
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CircleShape
@@ -26,7 +35,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -96,52 +111,6 @@ fun ScrollToTopLayout(
 @Composable
 fun ScrollToTopLayout(
     gridState: LazyStaggeredGridState,
-    scrollToTopButtonPadding: PaddingValues,
-    modifier: Modifier = Modifier,
-    grid: @Composable () -> Unit
-) {
-    val scope = rememberCoroutineScope()
-
-    Box(modifier = modifier) {
-        grid()
-
-        val showButton = remember {
-            derivedStateOf {
-                gridState.firstVisibleItemIndex > 0
-            }
-        }
-
-        AnimatedVisibility(
-            visible = showButton.value,
-            enter = fadeIn(),
-            exit = fadeOut(),
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(scrollToTopButtonPadding)
-        ) {
-            FilledTonalButton(
-                onClick = {
-                    scope.launch {
-                        gridState.scrollToItem(0)
-                    }
-                }
-            ) {
-                Icon(
-                    imageVector = Icons.Default.ArrowUpward,
-                    contentDescription = stringResource(id = R.string.to_top)
-                )
-
-                Spacer(modifier = Modifier.width(8.dp))
-
-                Text(text = stringResource(id = R.string.to_top))
-            }
-        }
-    }
-}
-
-@Composable
-fun ScrollToTopLayout(
-    gridState: LazyGridState,
     scrollToTopButtonPadding: PaddingValues,
     modifier: Modifier = Modifier,
     grid: @Composable () -> Unit
