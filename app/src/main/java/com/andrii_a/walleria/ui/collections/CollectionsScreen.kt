09cf043963ad56bb0e9d2221c53bf3df324e97a7
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2025-02-12 23:42:15 +0100

    Replaced CollectionsStaggeredGrid with CollectionsGridContent in CollectionsScreen.

--- app/src/main/java/com/andrii_a/walleria/ui/collections/CollectionsScreen.kt
+++ app/src/main/java/com/andrii_a/walleria/ui/collections/CollectionsScreen.kt
@@ -1,6 +1,5 @@
 package com.andrii_a.walleria.ui.collections
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -17,10 +16,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.andrii_a.walleria.R
-import com.andrii_a.walleria.ui.common.components.CollectionsStaggeredGrid
+import com.andrii_a.walleria.ui.common.components.CollectionsGridContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,11 +55,10 @@ fun CollectionsScreen(
         },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
-        CollectionsStaggeredGrid(
-            lazyCollectionItems = lazyCollectionItems,
+        CollectionsGridContent(
+            collectionItems = lazyCollectionItems,
             contentPadding = innerPadding,
-            onCollectionClick = { onEvent(CollectionsEvent.SelectCollection(it)) },
-            modifier = Modifier.padding(16.dp)
+            onCollectionClick = { onEvent(CollectionsEvent.SelectCollection(it)) }
         )
     }
 }
