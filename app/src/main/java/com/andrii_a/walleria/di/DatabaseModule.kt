bc3323c62b4b52638a7bf26e7b37132fe598138f
Andrii Andrushchenko <aw.andrushchenko@gmail.com>
2025-05-07 16:27:17 +0200

    Replaced deprecated fallbackToDestructiveMigration method in DatabaseModule.kt with a newer one.

--- app/src/main/java/com/andrii_a/walleria/di/DatabaseModule.kt
+++ app/src/main/java/com/andrii_a/walleria/di/DatabaseModule.kt
@@ -8,7 +8,7 @@ import org.koin.dsl.module
 val databaseModule = module {
     single<WalleriaDatabase> {
         Room.databaseBuilder(androidApplication(), WalleriaDatabase::class.java, "walleria_db")
-            .fallbackToDestructiveMigration()
+            .fallbackToDestructiveMigration(true)
             .build()
     }
 
